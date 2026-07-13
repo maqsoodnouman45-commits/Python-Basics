5a2fe31a81967dc24614325a45cb14ebdb695bb4
maqsoodnouman45-commits <maqsoodnouman45@gmail.com>
2026-07-13 19:04:33 +0500

    Create python poem.py
    A poem in Python to show the use case of PRINT STATEMENT using Escape Sequence

---
+++ python poem.py
@@ -0,0 +1,21 @@
+#Code for a POEM in Python
+print("""No brackets lock the logic tight,
+No semicolons end the night.
+With clean layout and elegant space,
+The code flows down with quiet grace.
+import the tools you wish to see,
+From math to data energy.
+A simple print() to speak your mind,
+The cleanest syntax you can find.
+if logic finds a truthful way,
+The indented block will have its say.
+for loops that spin through list and keys,
+With automated, human ease.
+Exceptions caught with try and except,
+No silent bugs where secrets crept.
+A snake that does not strike or bite,
+But guides the builder through the night.
+So type your script and watch it run,
+From simple loops to worlds begun.
+For pure simplicity is key,
+In Python's clear philosophy.""")
