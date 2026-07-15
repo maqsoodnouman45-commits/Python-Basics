8c08dbf4214b4cb053ab2bb5d0859dae6be563fb
maqsoodnouman45-commits <maqsoodnouman45@gmail.com>
2026-07-15 18:39:48 +0500

    Create Variables Declaration & Overwriting.py

---
+++ Variables Declaration & Overwriting.py
@@ -0,0 +1,14 @@
+#Predict the final printed output of this script before running it
+x = 10
+y = 5
+x = x + y
+y = x - y
+x = x - y
+print("x is:", x)
+print("y is:", y)
+
+#Output will be
+#x is: 5
+#y is: 10
+
+#Variables are containers that store data in it
