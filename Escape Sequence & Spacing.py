6508e2d3ec219d555267541240420a42d232a04f
maqsoodnouman45-commits <maqsoodnouman45@gmail.com>
2026-07-15 18:27:46 +0500

    Create Escape Sequence & Spacing.py

---
+++ Escape Sequence & Spacing.py
@@ -0,0 +1,6 @@
+#Using only a single print() statement, display the following receipt layout. Use tab (\t) and newline (\n) escape sequences to align the items and prices.
+print("Plaintext\n\nItem\t\t\tPrice\n---\t\t\t\t---\nBiryani\t\t\t350\nRaita\t\t\t50\n---\t\t\t\t---\nTotal\t\t\t400")
+
+#print() is used to display output
+#\t is used to give space between the entries
+#\n is used to create a new line
