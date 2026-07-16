eb286fc60917639610f3c8296aa2c6339612de82
maqsoodnouman45-commits <maqsoodnouman45@gmail.com>
2026-07-16 19:17:17 +0500

    Python Typecasting(Use of double-backslash)
    This repository explains the use of double-backlash to print the required output.

--- Use of Double-Backlash.py
+++ Use of Double-Backlash.py
@@ -1,4 +1,4 @@
 #Write a script that prints the following path exactly as it is, using the double-backslash escape sequence (\\) to bypass the error
 print("C:\\new_folder\\table\\script.p")
 
-#\\ is used to print a single backslash in the output.
+#\\ is used to print a single backslash in the output
