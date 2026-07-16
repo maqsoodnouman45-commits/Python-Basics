74e3f8c86b0a22eec05530dc0fcd61edd2bcd071
maqsoodnouman45-commits <maqsoodnouman45@gmail.com>
2026-07-16 18:52:51 +0500

    Python Escape Sequences
    This repository includes the explanation of Escape Sequence and use of \n & \t.

--- Escape Sequence & Spacing.py
+++ Escape Sequence & Spacing.py
@@ -1,4 +1,5 @@
 #Using only a single print() statement, display the following receipt layout. Use tab (\t) and newline (\n) escape sequences to align the items and prices.
+
 print("Plaintext\n\nItem\t\t\tPrice\n---\t\t\t\t---\nBiryani\t\t\t350\nRaita\t\t\t50\n---\t\t\t\t---\nTotal\t\t\t400")
 
 #print() is used to display output
