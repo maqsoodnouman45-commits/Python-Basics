41486893ff0cfa143b540ef24920bbd21893fb08
maqsoodnouman45-commits <maqsoodnouman45@gmail.com>
2026-07-16 19:00:24 +0500

    Python Print Statement
    This repository prints nd describes the use of \\ , \" and \.

--- Print Statement.py
+++ Print Statement.py
@@ -1,3 +1,3 @@
-#Write a script that uses a single print() function to display this exact sentence.
+#Write a script that uses a single print() function to display this exact sentence
 
 print("He said, \"It's a beautiful day for coding, isn't it?\"")
