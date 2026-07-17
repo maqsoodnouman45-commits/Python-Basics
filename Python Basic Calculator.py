85d13657e9c48a6c5ee61a8c5e5cdce3c54a49bb
maqsoodnouman45-commits <maqsoodnouman45@gmail.com>
2026-07-17 19:20:22 +0500

    Python Basic Calculator
    This repository gives code for a basic Python Calculator performing basic mathematical operations on two variables.

---
+++ Python Basic Calculator.py
@@ -0,0 +1,13 @@
+#Basic Python Calculator
+#Create a calculator capable of performing basic arithmetic operations like addition, subtraction, multiplication, and division.
+
+no_1=9
+no_2=5
+
+print("Addition of two numbers is:",no_1 + no_2) # Addition
+print("Subtraction of two numbers is:",no_1 - no_2) # Subtraction
+print("Multiplication of two numbers is:",no_1 * no_2) # Multiplication
+print("Division of two numbers is:",no_1 / no_2) # Division
+print("Modulus of two numbers is:",no_1 % no_2) # Modulus
+print("Floor Division of two numbers is:",no_1 // no_2) # Floor Division
+print("Exponential form of two numbers is:",no_1 ** no_2) # Exponential
