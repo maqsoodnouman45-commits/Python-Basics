708f67ade2bb68f0e654404b5982849cd81ec153
maqsoodnouman45-commits <maqsoodnouman45@gmail.com>
2026-07-18 20:35:34 +0500

    Python Practice(Profile Card)
    This repository shows how a specific layout is printed using input from user and escape sequences.

---
+++ Practice Question 1.py
@@ -0,0 +1,12 @@
+#Elegant Profile Card
+
+#Write a script that asks the user for three separate inputs:
+#Their full name (String)
+#Their current city (String)
+#Their favorite food (String)
+#Using only one single print() statement and escape sequences (\n and \t) to display the desired layout.
+
+name=input("Enter FuLL Name: ")
+city=input("Enter Current City: ")
+food=input("Enter your favourite food: ")
+print("NAME:\t\t\t",name,"\n""CITY:\t\t\t",city,"\n""FOOD:\t\t\t",food)
