98777ffdc36a9f57878ac7b0d5f2d6302e3e0c66
maqsoodnouman45-commits <maqsoodnouman45@gmail.com>
2026-07-20 21:24:34 +0500

    Python String Slicing
    This repository shows the concept of string slicing using array and also the use of len().

---
+++ String Slicing.py
@@ -0,0 +1,10 @@
+#String Slicing
+
+name="Nouman"
+print(name[0:1])
+print(name[0:2])
+print(name[0:3])
+print(name[0:4])
+print(name[0:5])
+print(name[0:6])
+print(len(name)) #Use of len() function to get the length of string
